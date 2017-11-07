63a9caa23dee54c40ebd69deee70d8fa639f03c9
monkypuzzle <paul.newsam@gmail.com>
2017-11-07 12:19:15 -0800

    Add first panel component with initial styling on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Hero from '../components/Hero'
+import React from 'react';
+import Link from 'gatsby-link';
+import Hero from '../components/Hero';
+import styled from 'styled-components';
 
 const IndexPage = () => (
   <div>
     <Hero />
+    <section className="section awd-grey">
+      <div className="container">
+        <h2 className="subtitle">A business card and an address...</h2>
+      </div>
+    </section>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
