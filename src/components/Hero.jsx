23a13914334258c14cfeffcb7edb118f18bed8f7
monkypuzzle <paul.newsam@gmail.com>
2017-11-07 11:57:40 -0800

    Add basic Hero component for home page

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Hero = () => {
+  return (
+    <section className="hero is-info is-large">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Hi, we're Anduin.</h1>
+          <h2 className="subtitle">
+            We build modern websites for growing businesses in professional
+            sectors.
+          </h2>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,13 @@ export class Navbar extends Component {
   render() {
     return (
       <nav className="navbar is-primary">
-        <div className="navbar-brand" />
+        <div className="navbar-brand">
+          <div className="navbar-burger">
+            <span />
+            <span />
+            <span />
+          </div>
+        </div>
         <div className="navbar-menu">
           <div className="navbar-start" />
           <div className="navbar-end">
