394ac5095b6283f661c5e14681bc9cc70239ebb4
monkypuzzle <paul.newsam@gmail.com>
2017-11-24 10:45:42 -0800

    Adds Google Analytics via plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-110166367-1',
+        anonymize: true,
+      },
+    },
+  ],
 };
