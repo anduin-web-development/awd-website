b36b435564be03fadc81a5a391257952653837d9
monkypuzzle <paul.newsam@gmail.com>
2017-11-29 10:36:54 -0800

    Updates Paul Bio

--- src/components/Team/PaulProfile.jsx
+++ src/components/Team/PaulProfile.jsx
@@ -16,19 +16,17 @@ const PaulProfile = props => {
         />
         <div className="content">
           <p>
-            Paul has a B.A. in Biology and English from U.C. Berkeley. Following
-            graduation, he conducted biological surveys around the Western U.S.
-            for organizations like USGS, Texas A&M University, and Bird
-            Conservancy of the Rockies. He was first exposed to web development
-            during a stint at a Sourcery Technologies, a SaaS tech startup in
-            San Francisco, where he led the data-processing effort.
+            Paul has built websites for businesses in retail, biotech, and
+            finance. Before Anduin, he led the data-processing effort at
+            Sourcery Technologies, a SaaS tech startup in San Francisco.
           </p>
           <p>
-            He trained in web development at DevBootcamp's San Diego campus in
-            2017, and since then he's built websites for businesses in retail,
-            finance, and biotech. He has experience developing applications in
-            Ruby on Rails and React. In his spare time, he likes to read, hike,
-            and write plays.
+            Formerly, Paul worked as a biologist conducting field surveys around
+            the Western U.S. for organizations like USGS, Texas A&M University,
+            and Bird Conservancy of the Rockies. He holds a degree in Biology
+            and English from U.C. Berkeley and trained in web development at Dev
+            Bootcamp. In his spare time, he likes to read, hike, and write
+            plays.
           </p>
           <a href="mailto:paul@anduin.io">
             <i className="fa fa-envelope fa-lg" aria-hidden="true" />
